694c22cc87e66abfc5e23cc720e7e4b777ca8479
Sammy H. jr <132605016+SammyHJr@users.noreply.github.com>
2025-03-07 22:06:35 +0100

    Hello

---
+++ app/src/main/java/com/example/weatherappkotlin/WeatherApp.kt
@@ -0,0 +1,10 @@
+package com.example.weatherappkotlin
+
+import android.util.Log
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherApp() {
+    Text("Lets build a weatherAPP")
+}
